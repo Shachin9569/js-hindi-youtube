92a379d74d65a002ee537eff7ed145ba9d27a6eb
Shachin9569 <sachintiwari956971@gmail.com>
2026-05-23 06:45:47 +0530

    Update test.js

--- .devcontainer/01_Basics/test.js
+++ .devcontainer/01_Basics/test.js
@@ -1,1 +1,5 @@
-console.log("Shachin Tiwari");
+let addName = "Pink City";
+const  addPass = "123";
+var = addCustomer = "Shachin";
+cityName = "jaipur";
+console.table([addName,addPass,addCustomer,cityName]);
